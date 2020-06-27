2cb8ab4dd8cb20bbdb70839a01c0b2a76cd0ba4a
uncleankiwi <feldtspirit@gmail.com>
2020-06-27 13:33:30 -0400

    -

--- jtools/src/replaceTextLoop/ReplaceUI.java
+++ jtools/src/replaceTextLoop/ReplaceUI.java
@@ -1,10 +1,30 @@
 package replaceTextLoop;
 
-public class ReplaceUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+//UIWrapper - HBox
+//	txtOutput - TextField
+//	filesBox - VBox
+//		sourceBox
+//		searchBox
+//		replaceBox
+//		outputBox
+//		btnStart
+
+//FileBox - HBox
+//	leftBox - VBox
+//		txtInstruction - Label
+//		txtFile - Label
+//	btnOpen
+
+public class ReplaceUI extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
+
